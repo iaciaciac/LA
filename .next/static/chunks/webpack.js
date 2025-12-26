fe0bef71a1a88048772ef9eb92578a298767407e
AKSTUDIO <akstudio@AK-Studio-Mac-mini.local>
2025-12-27 00:56:11 +0800

    Revert DAMN page and fix gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8edb7092e77e1075"; }
+/******/ 		__webpack_require__.h = function() { return "5c97138f3f4bffd0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,35 +1,20 @@
+
 import React from 'react';
 import Navbar from './components/Navbar';
-import PhotoGallery from './components/PhotoGallery';
-import { client } from '../sanity/lib/client';
-import groq from 'groq';
+import Ahometop from './components/home_look_m/Ahometop';
+import Ahometopb from './components/home_look_m/Ahometopb';
 
-function Photos({ photos }) {
+
+
+
+function Photos() {
   return (
-    <div className="min-h-screen bg-white dark:bg-black transition-colors duration-500">
+    <div>
       <Navbar />
-      <div className="pt-20 pb-10 px-6 md:px-16 lg:px-40 2xl:px-48">
-        <h1 className="text-4xl font-extrabold tracking-tight mb-4">Photos</h1>
-        <p className="text-gray-500 dark:text-gray-400 font-light text-lg">
-          Captured moments and visual stories.
-        </p>
-      </div>
-      <PhotoGallery photos={photos} />
+      <Ahometop />
+      <Ahometopb />
     </div>
   );
-}
-
-export async function getStaticProps() {
-  const photos = await client.fetch(groq`
-    *[_type == "photo"] | order(_createdAt desc)
-  `);
-
-  return {
-    props: {
-      photos
-    },
-    revalidate: 60
-  };
 }
 
 export default Photos;
